d45f6c6edaa7e4eef18ddeec3d1038ba6b20e08a
Casey Levin <clevin8080@yahoo.com>
2019-09-09 22:34:07 -0600

    updating javascript still wont get the keys top function..

--- JavaScript/game.js
+++ JavaScript/game.js
@@ -1,15 +1,29 @@
-// Setting variables
-var Winners = 0;
-var Losers = 0;
-var userGuesses = 10;
-var letters = undefined;
-var selection = [];
-
-// computer generated guesses
-var computerGuesses = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-// compouter randomization
-var computerGenerate = computerGuesses[Math.floor(Math.random() * computerGuesses.length)];
-// When user presses a letter on keyboard
-document.onkeyup = function() {
-
+// array of options / selectors
+var alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ];
+// variables for wins losses and guesses made by user and comnputer
+var win = 0;
+var loss = 0;
+var left = 10;
+var made = []
+var userAttempt = document.onkeyup;
+var roboAttempt = alpha[Math.floor(Math.random() * alpha.length)];
+// inputing function for the document.onkeyup
+document.onkeyup = function(event) {
+    userAttempt = event.key;
+    made.push(userAttempt);
+    if (roboAttempt === userAttempt) {
+        win++;
+        left = 10;
+        made = [];
+    }
+    if (roboAttempt = userAttempt) {
+        left--;
+    }
+    if (left == 0) {
+        loss++;
+        left = 10;
+        made = [];
+    }
+    // retaining info from index.html
+    document.getElementById(".game");
 }
